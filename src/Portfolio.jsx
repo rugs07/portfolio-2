8d273e97c7ac0086c416029d4dfa072637c41b2f
aspiringDev22 <anirudhs00.22@gmail.com>
2023-08-09 10:39:19 +0530

    added parallax-effect on projects

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,24 +1,9 @@
 import { Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import HomeLoader from "./components/HomeLoader";
-import Navbar from "./components/Navbar";
-import homebg from "./assets/home.png";
-import Home from "./components/Home";
-import Projects from "./components/Projects";
+
 
 const Portfolio = () => {
-  const [showloader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setShowLoader(false);
-    }, 2000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  });
 
   return (
     <Box>
