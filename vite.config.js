c17d2bb9639114fb28265047116b70a45dbddda9
rugs07 <rugwed07@gmail.com>
2024-06-20 15:03:57 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@mui/styles'], // Specify @mui/styles as an external module
+      external: /^@mui\/styles/, // Regular expression to match @mui/styles and its submodules
     },
   },
 });
